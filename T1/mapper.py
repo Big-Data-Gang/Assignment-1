c1f3584925c10e40c92ee19412d6095168bfb71c
ArvindAROO <i.arvindkrishna@gmail.com>
2021-09-09 23:30:41 +0530

    Added mapper code for T1

--- T1/mapper.py
+++ T1/mapper.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 
 import json
+import sys
+import datetime
+
+def checkConditions(myDict:dict = None):
+    if myDict == None:
+        return None
+    if (any([i in myDict["Description"] for i in ["lane blocked" , "shoulder blocked" , "overturned vehicle"]])) and (myDict["Severity"] >=2) and (myDict["Sunrise_Sunset"] == "Night") and (myDict["Visibility(mi)"] <= 10) and (myDict["Precipitation(in)"] >= 0.2) and (any([i in myDict["Weather_Condition"] for i in ["Heavy Snow", "Thunderstorm", "Heavy Rain", "Heavy Rain Showers", "Blowing Dust"]])):
+        return True
+    return False
+
+
+
+for line in sys.stdin:
+    myDict = json.loads(line)
+    status = checkConditions(myDict)
+    if status:
+        time = datetime.datetime.strptime(myDict["Start_Time"], "%Y-%m-%d %H:%M:%S")
+        print(time.hour, 1) 
+   
