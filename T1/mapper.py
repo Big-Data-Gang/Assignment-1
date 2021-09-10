937faba9525b6f2f5fff9671b214ddb1dc1aafea
thesuhas <suhastvs@gmail.com>
2021-09-10 20:19:57 +0530

    fixed T1

--- T1/mapper.py
+++ T1/mapper.py
@@ -4,11 +4,20 @@ import json
 import sys
 import datetime
 
+
 def checkConditions(myDict:dict = None):
     if myDict == None:
         return None
-    if (any([i in myDict["Description"] for i in ["lane blocked" , "shoulder blocked" , "overturned vehicle"]])) and (myDict["Severity"] >=2) and (myDict["Sunrise_Sunset"] == "Night") and (myDict["Visibility(mi)"] <= 10) and (myDict["Precipitation(in)"] >= 0.2) and (any([i in myDict["Weather_Condition"] for i in ["Heavy Snow", "Thunderstorm", "Heavy Rain", "Heavy Rain Showers", "Blowing Dust"]])):
-        return True
+    
+    if float("nan") in list(myDict.values()):
+        return False
+
+    try:
+        if (any([i in myDict["Description"].lower() for i in ["lane blocked" , "shoulder blocked" , "overturned vehicle"]])) and (myDict["Severity"] >=2) and (myDict["Sunrise_Sunset"].lower() == "night") and (myDict["Visibility(mi)"] <= 10) and (myDict["Precipitation(in)"] >= 0.2) and (any([i == myDict["Weather_Condition"].lower() for i in ["heavy snow", "thunderstorm", "heavy rain", "heavy rain showers", "blowing dust"]])):
+            time = datetime.datetime.strptime(myDict["Start_Time"], "%Y-%m-%d %H:%M:%S")
+            return True
+    except:
+        return False
     return False
 
 
@@ -18,9 +27,9 @@ for line in sys.stdin:
     status = checkConditions(myDict)
     if status:
         time = datetime.datetime.strptime(myDict["Start_Time"], "%Y-%m-%d %H:%M:%S")
-        #print(time.hour, type(time.hour))
         if time.hour < 12:
             print("0" + str(time.hour), 1)
         else:
             print(time.hour, 1) 
+    
    
