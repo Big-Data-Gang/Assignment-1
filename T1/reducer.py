3c744d7c9d8585c323ecf2a9e63b8a33f891094b
thesuhas <suhastvs@gmail.com>
2021-09-11 11:56:50 +0530

    removed redundant data from reducer

--- T1/reducer.py
+++ T1/reducer.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python3
   
-#from operator import itemgetter
 import sys
   
 current_hour = None
 current_count = 0
 hour = None
   
-# read the entire line from STDIN
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
-    # slpiting the data on the basis of tab we have provided in mapper.py
     try:
         hour, count = line.split(' ', 1)
     except:
         continue
-    # convert count (currently a string) to int
-    try:
-        count = int(count.strip())
-        hour = int(hour.strip())
-    except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
-        continue
-  
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: hour) before it is passed to the reducer
+    # try:
+    #     count = int(count.strip())
+    #     hour = int(hour.strip())
+    # except ValueError:
+    count = int(count.strip())
+    hour = int(hour.strip())
+
     if current_hour == hour:
         current_count += count
     else:
@@ -36,6 +29,5 @@ for line in sys.stdin:
         current_count = count
         current_hour = hour
   
-# do not forget to output the last hour if needed!
 if current_hour == hour:
     print(f"{current_hour} {current_count}")
