db653ba32f1db6da7eb4157660f566c41810513e
thesuhas <suhastvs@gmail.com>
2021-09-11 11:51:39 +0530

    changed to nested if

--- T1/mapper.py
+++ T1/mapper.py
@@ -4,6 +4,14 @@ import json
 import sys
 import datetime
 
+def checkDesc(string):
+    desc = ["lane blocked" , "shoulder blocked" , "overturned vehicle"]
+
+    for i in desc:
+        if i in string:
+            return True
+    return False
+
 
 def checkConditions(myDict:dict = None):
     if myDict == None:
@@ -13,9 +21,15 @@ def checkConditions(myDict:dict = None):
         return False
 
     try:
-        if (any([i in myDict["Description"].lower() for i in ["lane blocked" , "shoulder blocked" , "overturned vehicle"]])) and (myDict["Severity"] >=2) and (myDict["Sunrise_Sunset"].lower() == "night") and (myDict["Visibility(mi)"] <= 10) and (myDict["Precipitation(in)"] >= 0.2) and (any([i == myDict["Weather_Condition"].lower() for i in ["heavy snow", "thunderstorm", "heavy rain", "heavy rain showers", "blowing dust"]])):
-            time = datetime.datetime.strptime(myDict["Start_Time"], "%Y-%m-%d %H:%M:%S")
-            return True
+        #if (any([i in myDict["Description"].lower() for i in ["lane blocked" , "shoulder blocked" , "overturned vehicle"]])) and (myDict["Severity"] >=2) and (myDict["Sunrise_Sunset"].lower() == "night") and (myDict["Visibility(mi)"] <= 10) and (myDict["Precipitation(in)"] >= 0.2) and (any([i == myDict["Weather_Condition"].lower() for i in ["heavy snow", "thunderstorm", "heavy rain", "heavy rain showers", "blowing dust"]])):
+            #return True
+        if checkDesc(myDict['Description'].lower()):
+            if myDict['Severity'] >= 2:
+                if myDict['Sunrise_Sunset'] == 'Night':
+                    if myDict['Visibility(mi)'] <= 10:
+                        if myDict['Precipitation(in)'] >= 0.2:
+                            if myDict['Weather_Condition'].lower() in ["heavy snow", "thunderstorm", "heavy rain", "heavy rain showers", "blowing dust"]:
+                                return True
     except:
         return False
     return False
